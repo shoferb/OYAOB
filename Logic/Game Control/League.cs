6df5b3788897d792f21fd95cd66b49c8207f4de9
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-04-05 21:27:58 +0300

    create packages for all the project create classes og User, GemeContol, Notification and Log

---
+++ Logic/Game Control/League.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.Logic.Game_Control
+{
+    class League
+    {
+    }
+}
