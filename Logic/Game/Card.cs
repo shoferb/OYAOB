bb7e283bf21dbe34f9fc91ad6a207e727871399e
Aviv Gueta <guetaaviv@gmail.com>
2017-04-07 17:19:17 +0300

    git ignore fix and Card dup

---
+++ Logic/Game/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem
+{
+    public class Card
+    {
+
+        private int _value { set; get; }
+        private enum _cards
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades,
+        };
+
+        public Card(int val)
+        {
+            this._value = val;
+        }
+    }
+}
