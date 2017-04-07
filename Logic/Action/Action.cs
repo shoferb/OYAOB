2da9a54d8c5ff325566f5d8e1ebcda585b4cf731
Aviv Gueta <guetaaviv@gmail.com>
2017-04-07 11:58:57 +0300

    added abstract Action class

---
+++ Logic/Action/Action.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TexasHoldem.Logic.User;
+
+namespace TexasHoldem.Logic.Action
+{
+    public abstract class Action
+    {
+
+        private Player _player;
+        private int _gameID;
+
+        public Player Player { get => _player; set => _player = value; }
+        public int GameID { get => _gameID; set => _gameID = value; }
+
+        public abstract void DoAction();
+
+    }
+}
