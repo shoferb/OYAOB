7ca24fa7cefe72b890c9c183b5e7c0db3b575df4
miamiHits <yardench@gmail.com>
2017-04-07 13:35:51 +0300

    small shit

--- Logic/Game/GameRoom.cs
+++ Logic/Game/GameRoom.cs
@@ -5,12 +5,7 @@ namespace TexasHoldem.Logic.Game
 {
     abstract class GameRoom
     {
-        private string name;
-        private int sb;
-        private int bb;
-        private int minMoney;
-        private int maxMoney;
-
+ 
         private string _name { set; get; }
         private int _smallBlind { set; get; }
         private int _bigBlind { set; get; }
@@ -26,15 +21,6 @@ namespace TexasHoldem.Logic.Game
             this._minEnterMoney = minMoney;
             this._maxEnterMoney = maxMoney;
             this._gameNumber = gameNumber;
-        }
-
-        public GameRoom(string name, int sb, int bb, int minMoney, int maxMoney)
-        {
-            this.name = name;
-            this.sb = sb;
-            this.bb = bb;
-            this.minMoney = minMoney;
-            this.maxMoney = maxMoney;
         }
 
         private void Fold()
