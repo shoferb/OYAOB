cb1381d847473063f7d28823361d64dc3206f04b
Aviv Gueta <guetaaviv@gmail.com>
2017-04-07 17:56:56 +0300

    added isExist function and cunstructor in ReplayManager

--- Logic/Replay/ReplayManager.cs
+++ Logic/Replay/ReplayManager.cs
@@ -10,7 +10,34 @@ namespace TexasHoldem.Logic.Replay
     {
         private List<GameReplay> _gamesActions;
 
+        public ReplayManager()
+        {
+            _gamesActions = new List<GameReplay>();
+        }
+
         public List<GameReplay> GamesActions { get => _gamesActions; set => _gamesActions = value; }
+
+        public bool AddGameReplay(GameReplay gr)
+        {
+            if (gr.GameNumber < 0 || gr.GameRoomID < 0 || isExist(gr))
+            {
+                return false;
+            }
+            _gamesActions.Add(gr);
+            return true;
+        }
+
+        private bool isExist(GameReplay gr)
+        {
+            foreach (GameReplay g in _gamesActions)
+            {
+                if (g.RightGame(gr.GameRoomID, gr.GameNumber))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
         public bool ReplayGame(int gameRoomID, int gameNumber) {
 
