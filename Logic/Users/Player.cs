0e136f74aed368c9155de4221e95d6e53762fc25
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-04-08 09:33:52 +0300

    player and spectetor

---
+++ Logic/Users/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.Logic.Users
+{
+    public class Player : Spectetor
+    {
+        private bool isActive;
+
+        public Player(int id, string name, string memberName, int password, int points, int money,String email, int gameId, bool isActive) : base(id, name, memberName, password, points, money,email,gameId)
+        {
+            this.isActive = isActive;
+            //sads
+        }
+
+        //getter setter
+        public bool IsActive
+        {
+            get
+            {
+                return isActive;
+            }
+
+            set
+            {
+                isActive = value;
+            }
+        }
+    }
+}
