33b5c35a3aef1ef83f093d36c7b9f4985416ff09
Aviv Gueta <guetaaviv@gmail.com>
2017-04-08 11:35:40 +0300

    Various fixes to ReplayManager and GameReplay

--- Logic/Replay/GameReplay.cs
+++ Logic/Replay/GameReplay.cs
@@ -8,15 +8,18 @@ namespace TexasHoldem.Logic.Replay
     {
         public int _gameRoomID { get; set; }
         public int _gameNumber { get; set; }
+        private int _index;
         public List<Action> _actions { get; set; }
 
         public GameReplay()
         {
             _actions = new List<Action>();
+            _index = 0;
         }
 
         public GameReplay(int gameRoomID, int gameNumber)
         {
+            _index = 0;
             _gameRoomID = gameRoomID;
             _gameNumber = gameNumber;
             _actions = new List<Action>();
@@ -32,17 +35,20 @@ namespace TexasHoldem.Logic.Replay
             return (gameRoomID == _gameRoomID && gameNumber == _gameNumber);
         }
 
-        public bool ReplayGame()
+        public Action GetNextAction() //return current action or null if done
         {
-            if (_actions == null || _actions.Count == 0)
+            if (_actions == null || _actions.Count == 0 || _index >= _actions.Count)
             {
-                return false;
+                return null;
             }
-            foreach (Action a in _actions)
-            {
-                a.DoAction();
-            }
-            return true;
+            _index++;
+            return _actions.ElementAt(_index -1);
         }
+
+        public void StartOver()
+        {
+            _index = 0;
+        }
+
     }
 }
