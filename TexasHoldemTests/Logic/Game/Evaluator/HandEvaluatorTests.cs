48c8b36dc31a5fde4bff7186317fbfb12a41966f
Aviv Gueta <guetaaviv@gmail.com>
2017-04-10 09:27:33 +0300

    Two pairs tests

--- TexasHoldemTests/Logic/Game/Evaluator/HandEvaluatorTests.cs
+++ TexasHoldemTests/Logic/Game/Evaluator/HandEvaluatorTests.cs
@@ -68,9 +68,56 @@ namespace TexasHoldem.Logic.Game.Evaluator.Tests
         [TestMethod()]
         public void IsTwoPairTest()
         {
-            Assert.Fail();
+            _card1 = new Card(Suits.Clubs, 1);
+            _card2 = new Card(Suits.Hearts, 2);
+            _card3 = new Card(Suits.Clubs, 3);
+            _card4 = new Card(Suits.Diamonds, 3);
+            _card5 = new Card(Suits.Hearts, 1);
+            _card6 = new Card(Suits.Hearts, 7);
+            _card7 = new Card(Suits.Clubs, 8);
+
+            _cards[0] = _card1;
+            _cards[1] = _card2;
+            _cards[2] = _card3;
+            _cards[3] = _card4;
+            _cards[4] = _card5;
+            _cards[5] = _card6;
+            _cards[6] = _card7;
+
+            Assert.IsTrue(_evaluator.IsTwoPair(_cards));
+            _evalCards = _evaluator._relevantCards;
+            Assert.IsTrue(_evalCards.Contains(_card1));
+            Assert.IsTrue(_evalCards.Contains(_card5));
+            Assert.IsTrue(_evalCards.Contains(_card3));
+            Assert.IsTrue(_evalCards.Contains(_card4));
         }
 
+        [TestMethod()]
+        public void IsTwoPairTest2()
+        {
+            _card1 = new Card(Suits.Clubs, 1);
+            _card2 = new Card(Suits.Hearts, 4);
+            _card3 = new Card(Suits.Clubs, 3);
+            _card4 = new Card(Suits.Diamonds, 2);
+            _card5 = new Card(Suits.Hearts, 12);
+            _card6 = new Card(Suits.Hearts, 2);
+            _card7 = new Card(Suits.Clubs, 12);
+
+            _cards[0] = _card1;
+            _cards[1] = _card2;
+            _cards[2] = _card3;
+            _cards[3] = _card4;
+            _cards[4] = _card5;
+            _cards[5] = _card6;
+            _cards[6] = _card7;
+
+            Assert.IsTrue(_evaluator.IsTwoPair(_cards));
+            _evalCards = _evaluator._relevantCards;
+            Assert.IsTrue(_evalCards.Contains(_card4));
+            Assert.IsTrue(_evalCards.Contains(_card5));
+            Assert.IsTrue(_evalCards.Contains(_card6));
+            Assert.IsTrue(_evalCards.Contains(_card7));
+        }
         [TestMethod()]
         public void IsPairTest()
         {
