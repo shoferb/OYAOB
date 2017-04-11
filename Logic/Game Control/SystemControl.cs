e057b414089b0a73368f9e28f1a2dce5749e463d
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-04-11 16:11:46 +0300

    system control class

---
+++ Logic/Game Control/SystemControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.Logic.Game_Control
+{
+    class SystemControl
+    {
+        public struct DataLogin
+        {
+            public DataLogin(string username, string password)
+            {
+                UsernameData = username;
+                PasswordData = password;
+            }
+
+            public string UsernameData { get; set; }
+            public string PasswordData { get; set; }
+        }
+    }
+}
