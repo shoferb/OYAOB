618ff98ccd80ad60ac4eced6eb90631ba2fb55aa
Aviv Gueta <guetaaviv@gmail.com>
2017-04-13 20:57:09 +0300

    initillize Hand test

--- TexasHoldemTests/Logic/Game/HandOfPokerTests.cs
+++ TexasHoldemTests/Logic/Game/HandOfPokerTests.cs
@@ -5,12 +5,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TexasHoldem.Logic.Users;
 
 namespace TexasHoldem.Logic.Game.Tests
 {
     [TestClass()]
     public class HandOfPokerTests
     {
+        private HandOfPoker _hand;
+        private ConcreteGameRoom _room;
+        private Player _player1;
+        private Player _player2;
+        private Player _player3;
+
+        [TestInitialize()]
+        public void Initialize()   
+        {
+            _player1 = new Player(1, "one", "one", "one", 10, 100, "one@", 1, true);
+            _player2 = new Player(2, "two", "two", "two", 10, 100, "two@", 1, true);
+            _player3 = new Player(3, "three", "three", "three", 10, 100, "three@", 1, true);
+            List<Player> players = new List<Player>();
+            players.Add(_player1);
+            players.Add(_player2);
+            players.Add(_player3);
+            _room = new ConcreteGameRoom(players, 1);
+            _hand = new HandOfPoker(_room);
+        }
+
         [TestMethod()]
         public void HandOfPokerTest()
         {
