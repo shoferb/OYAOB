ddbd237812da9f6636ef9d8bb4745465e2688a3c
miamiHits <yardench@gmail.com>
2017-04-13 15:22:29 +0300

    added unit test for game room

---
+++ TexasHoldemTests/Logic/Game/ConcreteGameRoomTests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TexasHoldem.Logic.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.Logic.Game.Tests
+{
+    [TestClass()]
+    public class ConcreteGameRoomTests
+    {
+        [TestMethod()]
+        public void ConcreteGameRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddNewPublicCardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NextToPlayTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DelaerPositionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToCallTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateGameStateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearPublicCardsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateMaxCommittedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EndTurnTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ResetActionPosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoveChipsToPotTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PlayersInHandTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PlayersAllInTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AllDoneWithTurnTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void newSplitPotTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
