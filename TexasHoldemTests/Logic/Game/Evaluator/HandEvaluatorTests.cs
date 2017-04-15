036ba150a2c9d6a088a57dbc1eef53467cc8b213
Aviv Gueta <guetaaviv@gmail.com>
2017-04-15 09:39:27 +0300

    added tests

--- TexasHoldemTests/Logic/Game/Evaluator/HandEvaluatorTests.cs
+++ TexasHoldemTests/Logic/Game/Evaluator/HandEvaluatorTests.cs
@@ -733,6 +733,34 @@ namespace TexasHoldem.Logic.Game.Evaluator.Tests
         }
 
         [TestMethod()]
+        public void IsThreeOfAKindTest3()
+        {
+            _card1 = new Card(Suits.Clubs, 2);
+            _card2 = new Card(Suits.Hearts, 1);
+            _card3 = new Card(Suits.Clubs, 3);
+            _card4 = new Card(Suits.Diamonds, 4);
+            _card5 = new Card(Suits.Hearts, 9);
+            _card6 = new Card(Suits.Hearts, 2);
+            _card7 = new Card(Suits.Diamonds, 2);
+
+            _cards[0] = _card1;
+            _cards[1] = _card2;
+            _cards[2] = _card3;
+            _cards[3] = _card4;
+            _cards[4] = _card5;
+            _cards[5] = _card6;
+            _cards[6] = _card7;
+
+            Assert.IsTrue(_evaluator.IsThreeOfAKind(_cards));
+            _evalCards = _evaluator._relevantCards;
+            Assert.IsTrue(_evalCards.Contains(_card1));
+            Assert.IsTrue(_evalCards.Contains(_card5));
+            Assert.IsTrue(_evalCards.Contains(_card7));
+            Assert.IsTrue(_evalCards.Contains(_card6));
+            Assert.IsTrue(_evalCards.Contains(_card2));
+        }
+
+        [TestMethod()]
         public void IsTwoPairTest()
         {
             _card1 = new Card(Suits.Clubs, 1);
@@ -886,6 +914,34 @@ namespace TexasHoldem.Logic.Game.Evaluator.Tests
             _cards[6] = _card7;
 
             Assert.IsFalse(_evaluator.IsPair(_cards));
+        }
+
+        [TestMethod()]
+        public void IsPairTest4()
+        {
+            _card1 = new Card(Suits.Clubs, 4);
+            _card2 = new Card(Suits.Hearts, 1);
+            _card3 = new Card(Suits.Clubs, 9);
+            _card4 = new Card(Suits.Diamonds, 5);
+            _card5 = new Card(Suits.Hearts, 3);
+            _card6 = new Card(Suits.Hearts, 2);
+            _card7 = new Card(Suits.Clubs, 2);
+
+            _cards[0] = _card1;
+            _cards[1] = _card2;
+            _cards[2] = _card3;
+            _cards[3] = _card4;
+            _cards[4] = _card5;
+            _cards[5] = _card6;
+            _cards[6] = _card7;
+
+            Assert.IsTrue(_evaluator.IsPair(_cards));
+            _evalCards = _evaluator._relevantCards;
+            Assert.IsTrue(_evalCards.Contains(_card3));
+            Assert.IsTrue(_evalCards.Contains(_card4));
+            Assert.IsTrue(_evalCards.Contains(_card2));
+            Assert.IsTrue(_evalCards.Contains(_card6));
+            Assert.IsTrue(_evalCards.Contains(_card7));
         }
 
         [TestMethod()]
