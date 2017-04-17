19e351f1a2b69f9f824dce3411127a86ede15254
oded rojtblat <oded.rojtblat@gmail.com>
2017-04-17 22:28:24 +0300

    bla

---
+++ Logic/Notifications And Logs/SystemLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.Logic.Notifications_And_Logs
+{
+    public class SystemLog : Log
+    {
+        private String msg;
+
+        public SystemLog(int lodId,string msg) : base()
+        {
+            this.msg = msg;
+        }
+
+
+        //getter setter
+        public string Msg
+        {
+            get
+            {
+                return msg;
+            }
+
+            set
+            {
+                msg = value;
+            }
+        }
+    }
+}
