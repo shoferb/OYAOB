798e747c37c74598fc40ff6449b27439a251b9f3
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-04-18 14:57:13 +0300

    player - win, lose, getNew point impl win not complete yet

--- Logic/Users/Player.cs
+++ Logic/Users/Player.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TexasHoldem.Logic.Game;
+using TexasHoldem.Logic.Game_Control;
 
 namespace TexasHoldem.Logic.Users
 {
@@ -16,9 +17,14 @@ namespace TexasHoldem.Logic.Users
         public int _gameChip { get; set; }
 
         public string _lastAction { get; set; }
+
+        
+
         public Hand _hand;
         public enum playerMoves { Call, Check, Bet, Fold, Raise }
 
+        private int _winNum;
+        private int _loseNum;
 
         public Player(int totalChip, int gameChipComitted, int id, string name, string memberName, string password, int points, int money, String email,
             int roomId) : base(id, name, memberName, password, points, money, email, roomId)
@@ -31,6 +37,8 @@ namespace TexasHoldem.Logic.Users
             this._totalChip = totalChip;
             isPlayerActive = false;
             _hand = new Hand();
+            this._winNum = 0;
+            this._loseNum = 0;
 
         }
 
@@ -104,16 +112,77 @@ namespace TexasHoldem.Logic.Users
             this._totalChip -= chips;
             this._gameChip += chips;
         }
-        //TODO: orellie
-        public void Win(int amount)
+
+
+        //get amout won, inc winCounter + update point + check if the user is now the highest player
+        public bool Win(int amount)
         {
-
+            bool toReturn;
+            try
+            {
+                WinNum = WinNum + 1;
+                int newPoint = GetNewPoint();
+                Points = newPoint;
+                SystemControl sc = new SystemControl();
+                //int highestRank = sc.
+                //todo - need to chck if user is noe the hugest rank
+                toReturn = true;
+            }
+            catch (Exception e)
+            {
+                toReturn = false;
+            }
+            return toReturn;
         }
-        //TODO: orellie
-        public static void Lose()
+        //todo - yarden - why static?? remove for now
+        public  bool Lose()
         {
-
+            bool toReturn;
+            try
+            {
+                LoseNum = LoseNum + 1;
+                int newPoint = GetNewPoint();
+                Points = newPoint;
+                toReturn = true;
+            }
+            catch (Exception e)
+            {
+                toReturn = false;
+            }
+            return toReturn;
         }
 
+        public int GetNewPoint()
+        {
+            int calc = (int)(Money / 100);
+            int newPoint = (20 * ((5 * WinNum) - LoseNum)) + calc;
+            return newPoint;
+        }
+        public int WinNum
+        {
+            get
+            {
+                return _winNum;
+            }
+
+            set
+            {
+                _winNum = value;
+            }
+        }
+
+        public int LoseNum
+        {
+            get
+            {
+                return _loseNum;
+            }
+
+            set
+            {
+                _loseNum = value;
+            }
+        }
     }
+
 }
