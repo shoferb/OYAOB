0de0abdb5f44aa98c9647b36fb79fd8dd648ef66
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-04-28 18:48:56 +0300

    Spectetor error test fix

--- TexasHoldemTests/Logic/Users/SpectetorTests.cs
+++ TexasHoldemTests/Logic/Users/SpectetorTests.cs
@@ -14,34 +14,35 @@ namespace TexasHoldem.Logic.Users.Tests
         [TestMethod()]
         public void SpectetorTest()
         {
-            Spectetor toTest = new Spectetor(305077901,"orelie","orelie12",
-                "12345678",0,100,"orelie@post.bgu.ac.il",1);
-            Assert.AreEqual(toTest.Id,305077901);
-            toTest.Id = 305077902;
-            Assert.AreNotEqual(toTest.Id, 305077901);
-            Assert.AreEqual(toTest.Id, 305077902);
+
+            User user = new User(305077901, "orelie", "orelie12",
+                "12345678", 0, 100, "orelie@post.bgu.ac.il");
+            Spectetor toTest = new Spectetor(user, 1);
+            Assert.AreEqual(toTest.user.Id(),305077901);
+            toTest.user.EditId(305077902);
+            Assert.AreNotEqual(toTest.user.Id(), 305077901);
+            Assert.AreEqual(toTest.user.Id(), 305077902);
             Assert.AreEqual(toTest.RoomId,1);
-            toTest.RoomId = 2;
-            Assert.AreNotEqual(toTest.RoomId, 1);
-            Assert.AreEqual(toTest.RoomId, 2);
-            Assert.AreEqual(toTest.Name,"orelie");
-            toTest.Name = "Amir";
-            Assert.AreNotEqual(toTest.Name, "orelie");
-            Assert.AreEqual(toTest.Name, "Amir");
-            Assert.AreEqual(toTest.Points,0);
-            toTest.Points = 100;
-            Assert.AreNotEqual(toTest.Points, 0);
-            Assert.AreEqual(toTest.Points, 100);
-            Assert.AreEqual(toTest.MemberName,"orelie12");
-            toTest.MemberName = "orelie123";
-            Assert.AreNotEqual(toTest.MemberName, "orelie12");
-            Assert.AreEqual(toTest.MemberName, "orelie123");
-            Assert.AreEqual(toTest.Password,"12345678");
-            Assert.AreEqual(toTest.Email,"orelie@post.bgu.ac.il");
-            Assert.AreEqual(toTest.Money, 100);
-            toTest.Money = 10;
-            Assert.AreNotEqual(toTest.Money, 100);
-            Assert.AreEqual(toTest.Money, 10);
+           
+            Assert.AreEqual(toTest.RoomId, 1);
+            Assert.AreEqual(toTest.user.Name(),"orelie");
+            toTest.user.EditName("Amir");
+            Assert.AreNotEqual(toTest.user.Name(), "orelie");
+            Assert.AreEqual(toTest.user.Name(), "Amir");
+            Assert.AreEqual(toTest.user.Points(),0);
+            toTest.user.EditUserPoints(100);
+            Assert.AreNotEqual(toTest.user.Points(), 0);
+            Assert.AreEqual(toTest.user.Points(), 100);
+            Assert.AreEqual(toTest.user.MemberName(),"orelie12");
+            toTest.user.EditUserName( "orelie123");
+            Assert.AreNotEqual(toTest.user.MemberName(), "orelie12");
+            Assert.AreEqual(toTest.user.MemberName(), "orelie123");
+            Assert.AreEqual(toTest.user.Password(),"12345678");
+            Assert.AreEqual(toTest.user.Email(),"orelie@post.bgu.ac.il");
+            Assert.AreEqual(toTest.user.Money(), 100);
+            toTest.user.EditUserMoney(10);
+            Assert.AreNotEqual(toTest.user.Money(), 100);
+            Assert.AreEqual(toTest.user.Money(), 10);
         }
     }
 }
