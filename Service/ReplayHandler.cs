66ac4333757901a4501ff449ae55a5e6fe037d65
Aviv Gueta <guetaaviv@gmail.com>
2017-05-01 11:36:30 +0300

    added GetGameReplayForUser  ShowGameReplay  DeleteGameReplay to ReplayHandler class

--- Service/ReplayHandler.cs
+++ Service/ReplayHandler.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TexasHoldem.Logic.Replay;
 
 namespace TexasHoldem.Service
 {
-    class ReplayHandler
+    public class ReplayHandler
     {
+        private ReplayManager _RM = ReplayManager.ReplayManagerInstance;
+
+        public GameReplay GetGameReplayForUser(int gameRoomID, int gameNumber, int userID)
+        {
+            return _RM.GetGameReplayForUser(gameRoomID, gameNumber, userID);
+        }
+
+        public string ShowGameReplay(int gameRoomID, int gameNumber, int userID)
+        {
+            return _RM.ShowGameReplay(gameRoomID, gameNumber, userID);
+        }
+
+        public void DeleteGameReplay(int gameRoomID, int gameNumber)
+        {
+            _RM.DeleteGameReplay(gameRoomID, gameNumber);
+        }
 
     }
 }
