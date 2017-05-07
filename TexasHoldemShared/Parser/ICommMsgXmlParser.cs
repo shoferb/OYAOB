62e79289e7f2cc60b92a8c56d843923f1aeacd47
shoferb <shoferb@post.bgu.ac.il>
2017-05-07 19:16:36 +0300

    XML parsers generic - done

--- TexasHoldemShared/Parser/ICommMsgXmlParser.cs
+++ TexasHoldemShared/Parser/ICommMsgXmlParser.cs
@@ -1,8 +1,37 @@
-﻿namespace TexasHoldemShared.Parser
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+namespace TexasHoldemShared.Parser
 {
     public interface ICommMsgXmlParser
     {
-        string SerializeMsg(CommMessages.CommunicationMessage msg);
-        CommMessages.CommunicationMessage ParseString(string msg); //TODO: parse to exact msg type (GameDataMessage for example)
+
+        public static string SerializeMsg(object dataToSerialize)
+        {
+            if (dataToSerialize == null) return null;
+
+            using (StringWriter stringwriter = new System.IO.StringWriter())
+            {
+                var serializer = new XmlSerializer(dataToSerialize.GetType());
+                serializer.Serialize(stringwriter, dataToSerialize);
+                return stringwriter.ToString();
+            }
+        }
+
+        public static T ParseString<T>(string xmlText)
+        {
+            if (String.IsNullOrWhiteSpace(xmlText)) return default(T);
+
+            using (StringReader stringReader = new System.IO.StringReader(xmlText))
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                return (T)serializer.Deserialize(stringReader);
+            }
+        }
+
+       
+
+  //      string SerializeMsg(CommMessages.CommunicationMessage msg);
+    //    CommMessages.CommunicationMessage ParseString(string msg); //TODO: parse to exact msg type (GameDataMessage for example)
     }
 }
