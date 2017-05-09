4f780b1aa4730203b263d814df668b382d33355d
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-09 19:16:15 +0300

    LoginResponeCommMessage - create RegisterResponeCommMessage - create

---
+++ TexasHoldemShared/CommMessages/ServerToClient/RegisterResponeCommMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldemShared.CommMessages.ServerToClient
+{
+    public class RegisterResponeCommMessage : ResponeCommMessage
+    {
+        //todo - this class is te message responce we get when register 
+        public RegisterResponeCommMessage(int id, bool success, CommunicationMessage originalMsg) : base(id, success, originalMsg)
+        {
+        }
+
+        //visitor pattern
+        public override void Handle(IEventHandler handler)
+        {
+            handler.HandleEvent(this);
+        }
+    }
+}
