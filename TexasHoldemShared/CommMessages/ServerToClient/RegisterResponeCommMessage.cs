f176fcba0db64b42fac24216729ae38691a6f5a2
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-09 19:18:26 +0300

    RegisterResponeCommMessage - done

--- TexasHoldemShared/CommMessages/ServerToClient/RegisterResponeCommMessage.cs
+++ TexasHoldemShared/CommMessages/ServerToClient/RegisterResponeCommMessage.cs
@@ -9,8 +9,23 @@ namespace TexasHoldemShared.CommMessages.ServerToClient
     public class RegisterResponeCommMessage : ResponeCommMessage
     {
         //todo - this class is te message responce we get when register 
-        public RegisterResponeCommMessage(int id, bool success, CommunicationMessage originalMsg) : base(id, success, originalMsg)
+
+        private string name;
+        private string username;
+        private string password;
+        private string avatar;//- image path
+        private int money;
+        private string email;
+
+        public RegisterResponeCommMessage(int id, string _name, string _username, string _password,
+            string _avatar, int _money, string _email, bool success, CommunicationMessage originalMsg) : base(id, success, originalMsg)
         {
+            this.name = _name;
+            this.username = _username;
+            this.password = _password;
+            this.avatar = _avatar;
+            this.money = _money;
+            this.email = _email;
         }
 
         //visitor pattern
