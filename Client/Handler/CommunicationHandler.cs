1ce9d3e06e88e05a111fd0fab8d5463c0ae9093a
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-09 14:47:22 +0300

    CommunicationHandler -added

---
+++ Client/Handler/CommunicationHandler.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Net.Sockets;
+using TexasHoldemShared.Parser;
+
+namespace Client.Handler
+{
+    public class CommunicationHandler
+    {
+        protected int _userId;
+        protected readonly ConcurrentQueue<string> _receivedMsgQueue;
+        protected readonly ConcurrentQueue<string> _toSendMsgQueue;
+        protected TcpClient _socket;
+        protected NetworkStream _stream;
+        protected readonly string _server;
+        private bool _shouldClose;
+
+        public CommunicationHandler(string server)
+        {
+
+            _receivedMsgQueue = new ConcurrentQueue<string>();
+            _toSendMsgQueue = new ConcurrentQueue<string>();
+            _server = server;
+            _shouldClose = false;
+            _socket.ReceiveTimeout = 5000;
+
+        }
+        public void setUserId(int newId)
+        {
+            this._userId = newId;
+        }
+
+        private bool Connect()
+        {
+            try
+            {
+                // Create a TcpClient.
+                // connects to the specified port on the specified host.
+                Int32 port = 2000;
+                _socket = new TcpClient(_server, port);
+                // Get a client stream for reading and writing.
+                _stream = _socket.GetStream();
+                return true;
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                return false;
+            }
+        }
+        public string tryGetMsgReceived()
+        {
+            string msgToRet = string.Empty;
+            _receivedMsgQueue.TryDequeue(out msgToRet);
+            return msgToRet;
+        }
+        public void addMsgToSend(string msg)
+        {
+            _toSendMsgQueue.Enqueue(msg);
+        }
+
+        private void sendMessages()
+        {
+            while (!_shouldClose)
+            {
+                try
+                {
+                    string msg = string.Empty;
+                    if (!_toSendMsgQueue.IsEmpty)
+                    {
+                        _toSendMsgQueue.TryDequeue(out msg);
+                        // Translate the passed message into ASCII and store it as a Byte array.
+                        Byte[] data = System.Text.Encoding.UTF8.GetBytes(msg);
+                        // Send the message to the connected TcpServer. 
+                        _stream.Write(data, 0, data.Length);
+                    }
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("SocketException: {0}", e);
+
+                }
+            }
+        }
+
+        //ojo
+
+        //returns new msg as string, or eempty string if there's no new msg
+        private void receiveMessages()
+        {
+            byte[] buffer = new byte[1];
+            while (!_shouldClose)
+            {
+                IList<byte> data = new List<byte>();
+                var dataReceived = 0;
+                do
+                {
+                    try
+                    {
+                        dataReceived = _stream.Read(buffer, 0, 1);
+                        if (dataReceived > 0)
+                        {
+                            data.Add(buffer[0]);
+                        }
+                    }
+                    catch (Exception e) { }
+
+                } while (dataReceived > 0);
+
+                //add msg string to queue
+                if (data.Count > 0)
+                {
+                    _receivedMsgQueue.Enqueue(Encoding.UTF8.GetString(data.ToArray()));
+                }
+            }
+            close();
+
+        }
+
+
+
+        public bool close()
+        {
+            try
+            {
+                _shouldClose = true;
+                _stream.Close();
+                _socket.Close();
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+
+
+
+        public void Start()
+        {
+            Task task = new Task(receiveMessages);
+
+            if (Connect())
+            {
+                task.Start();
+                sendMessages();
+
+            }
+            else
+            {
+                Console.WriteLine("cant connect to server");
+            }
+        }
+
+    }
+}
