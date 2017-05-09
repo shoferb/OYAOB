e82411547a6f98c43bc57c63af59dd1829869381
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-09 23:28:37 +0300

    SearchCommMessage implement CommunicationMessage

--- TexasHoldemShared/CommMessages/ClientToServer/SearchCommMessage.cs
+++ TexasHoldemShared/CommMessages/ClientToServer/SearchCommMessage.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace TexasHoldemShared.CommMessages.ClientToServer
 {
-    class SearchCommMessage
+     public class SearchCommMessage : CommunicationMessage
     {
-    }
+    
+        public enum SearchField
+        {
+            ByUserName,
+            ByRoomId,
+            AllActiveGames,
+            AllSepctetorGame,
+            GamesUserCanJoin,
+            ByPotSize,
+            ByGameMode,
+            ByBuyInPolicy,
+            ByMinPlayer,
+            ByMaxPlayer,
+            ByStartingChip
+        }
+
+
+         public SearchCommMessage(int id) : base(id)
+         {
+         }
+
+         public override void Handle(IEventHandler handler)
+         {
+             throw new NotImplementedException();
+         }
+     }
 }
