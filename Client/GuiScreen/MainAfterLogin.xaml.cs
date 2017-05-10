264cbecddd69d197087554e2ff48d596130da8bc
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-10 11:20:40 +0300

    open fix

--- Client/GuiScreen/MainAfterLogin.xaml.cs
+++ Client/GuiScreen/MainAfterLogin.xaml.cs
@@ -53,6 +53,7 @@ namespace TexasHoldem.GuiScreen
         {
             EditUserInfo editUserInfo = new EditUserInfo(this,currUserId,cl);
             editUserInfo.Show();
+            
         }
 
         private void GameSearchMenuutton_Click(object sender, RoutedEventArgs e)
