2697e03afe6663051230b4d1e25e134d1398f50f
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-10 11:24:59 +0300

    back button function

--- Client/GuiScreen/SearchMenuScreen.xaml.cs
+++ Client/GuiScreen/SearchMenuScreen.xaml.cs
@@ -23,5 +23,10 @@ namespace Client.GuiScreen
         {
             InitializeComponent();
         }
+
+        private void Backbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
