1e8b9ac67fad687ad03649331980fbbf65073042
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-10 15:11:06 +0300

    finshing search responce message

--- TexasHoldemShared/CommMessages/ServerToClient/SearchResponseCommMessage.cs
+++ TexasHoldemShared/CommMessages/ServerToClient/SearchResponseCommMessage.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TexasHoldemShared.CommMessages.ClientToServer;
 
 namespace TexasHoldemShared.CommMessages.ServerToClient
 {
-    class SearchResponseCommMessage
+    class SearchResponseCommMessage : ResponeCommMessage
     {
+        public List<ClientGame> games;
+
+        public SearchResponseCommMessage(List<ClientGame> _games, int id, bool success, CommunicationMessage originalMsg) : base(id, success, originalMsg)
+        {
+            this.games = _games;
+        }
+
+        //visitor pattern
+        public override void Handle(IEventHandler handler)
+        {
+            handler.HandleEvent(this);
+        }
     }
 }
