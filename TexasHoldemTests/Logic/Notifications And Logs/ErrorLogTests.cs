d5c68917f94ead82797d2343ce05bc2aae005964
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-10 20:50:51 +0300

    error unit fix - seperete big test to sevral tests

--- TexasHoldemTests/Logic/Notifications And Logs/ErrorLogTests.cs
+++ TexasHoldemTests/Logic/Notifications And Logs/ErrorLogTests.cs
@@ -11,32 +11,47 @@ namespace TexasHoldem.Logic.Notifications_And_Logs.Tests
     [TestClass()]
     public class ErrorLogTests
     {
+       
+
         [TestMethod()]
-        public void ErrorLogTest()
+        public void ErrorLogTestNextIdSucces()
+        {
+            int logId1 = Log.getNextId();
+            
+            ErrorLog errorLog = new ErrorLog("error log test");
+            Assert.AreEqual(errorLog.LogId, logId1);
+        }
+
+
+
+        [TestMethod()]
+        public void LogTesNotSameIdSucces()
         {
             int logId1 = Log.getNextId();
             ErrorLog errorLog = new ErrorLog("error log test");
             int logId2 = Log.getNextId();
             ErrorLog errorLog2 = new ErrorLog("error log test2");
-            Assert.AreEqual(errorLog.Msg, "error log test");
-            Assert.AreNotEqual(errorLog.LogId,errorLog2.LogId);
-            Assert.AreEqual(errorLog.LogId, logId1);
-            Assert.AreEqual(errorLog2.LogId, logId2);
-            Assert.AreEqual(errorLog2.LogId, errorLog.LogId+1);
-            Assert.IsInstanceOfType(errorLog2,typeof(ErrorLog));
-            Assert.IsInstanceOfType(errorLog2, typeof(Log));
-            Assert.IsNotInstanceOfType(errorLog2, typeof(SystemLog));
+            Assert.AreNotEqual(errorLog.LogId, errorLog2.LogId);
+
+        }
+
+        [TestMethod()]
+        public void LogTesIncGood()
+        {
+            int logId1 = Log.getNextId();
+            ErrorLog errorLog = new ErrorLog("error log test");
+            int logId2 = Log.getNextId();
+            ErrorLog errorLog2 = new ErrorLog("error log test2");
+            Assert.AreEqual(errorLog2.LogId, errorLog.LogId + 1);
+
         }
 
         [TestMethod()]
         public void ToStringTest()
         {
             ErrorLog errorLog = new ErrorLog("error log test");
-            ErrorLog errorLog2 = new ErrorLog("error log test2");
             string toCheck1 = "Log Id is: " + errorLog.LogId + " msg is: " + errorLog.Msg;
-            string toCheck2 = "Log Id is: " + errorLog2.LogId + " msg is: " + errorLog2.Msg;
             Assert.AreEqual(errorLog.ToString(),toCheck1);
-            Assert.AreEqual(errorLog2.ToString(), toCheck2);
         }
     }
 }
