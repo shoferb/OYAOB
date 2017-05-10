b189ecd840ebdddced618eca9c4033ac44d74042
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-10 20:54:22 +0300

    removed unessery test

--- TexasHoldemTests/Logic/Notifications And Logs/NotificationTests.cs
+++ TexasHoldemTests/Logic/Notifications And Logs/NotificationTests.cs
@@ -11,28 +11,15 @@ namespace TexasHoldem.Logic.Notifications_And_Logs.Tests
     [TestClass()]
     public class NotificationTests
     {
-        [TestMethod()]
-        public void NotificationTest()
-        {
-            Notification toTest1 = new Notification(1,"new notificarion to test");
-            Notification toTest2 = new Notification(2, "new notificarion to test2");
-            Assert.AreEqual(toTest1.Msg, "new notificarion to test");
-            Assert.AreEqual(toTest2.Msg, "new notificarion to test2");
-            Assert.AreEqual(toTest1.RoomId, 1);
-            Assert.AreEqual(toTest2.RoomId, 2);
-            
-        }
+        
 
         [TestMethod()]
         public void ToStringTest()
         {
 
             Notification toTest1 = new Notification(1, "new notificarion to test");
-            Notification toTest2 = new Notification(2, "new notificarion to test2");
             string toCheck1 = "this is a notification with  massage: " + toTest1.Msg + "to room Id: " + toTest1.RoomId;
-            string toCheck2 = "this is a notification with  massage: " + toTest2.Msg + "to room Id: " + toTest2.RoomId;
             Assert.AreEqual(toTest1.ToString(), toCheck1);
-            Assert.AreEqual(toTest2.ToString(), toCheck2);
         }
     }
 }
