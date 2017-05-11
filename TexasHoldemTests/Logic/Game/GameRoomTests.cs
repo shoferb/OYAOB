e2b066008ac5d77eff246d18469364705c999cf5
Aviv Gueta <guetaaviv@gmail.com>
2017-05-11 07:18:03 +0300

    GameRoomTests relocation

---
+++ TexasHoldemTests/Logic/Game/GameRoomTests.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TexasHoldem.Logic.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.Logic.Game.Tests
+{
+    [TestClass()]
+    public class GameRoomTests
+    {
+        [TestMethod()]
+        public void GameRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddDecoratorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DoActionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindWinnerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddSpectetorToRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveSpectetorFromRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CanJoinTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsGameActiveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsSpectatableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsPotSizEqualTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsGameModeEqualTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsGameBuyInPolicyEqualTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsGameMinPlayerEqualTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsGameMaxPlayerEqualTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsGameMinBetEqualTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsGameStartingChipEqualTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPlayersInRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSpectetorInRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetMinPlayerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetMinBetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetMaxPlayerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPotSizeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetBuyInPolicyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetStartingChipTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameGameModeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetLeagueNameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
