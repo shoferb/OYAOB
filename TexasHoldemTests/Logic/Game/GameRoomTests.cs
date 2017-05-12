17903ea61dccbe87836feb9390f855f9fa14349d
Aviv Gueta <guetaaviv@gmail.com>
2017-05-12 11:13:54 +0300

    added DoActionStartGameTest

--- TexasHoldemTests/Logic/Game/GameRoomTests.cs
+++ TexasHoldemTests/Logic/Game/GameRoomTests.cs
@@ -104,6 +104,22 @@ namespace TexasHoldem.Logic.Game.Tests
         }
 
         [TestMethod()]
+        public void DoActionStartGameTest()
+        {
+            SetDecoratoresLimitNoSpectatores();
+            //already player user
+            Assert.IsFalse(gameRoom.DoAction(user1, ActionType.Join, 1000));
+            //new user not enough money in amount
+            Assert.IsFalse(gameRoom.DoAction(user2, ActionType.Join, 1000));
+            //new user not enough money in total
+            user2.EditUserMoney(10);
+            Assert.IsFalse(gameRoom.DoAction(user2, ActionType.Join, 2000));
+            //user with enough money
+            user2.EditUserMoney(10000);
+            Assert.IsTrue(gameRoom.DoAction(user2, ActionType.Join, 1500));
+        }
+
+        [TestMethod()]
         public void AddSpectetorToRoomTest()
         {
             //user that is a player in the room
