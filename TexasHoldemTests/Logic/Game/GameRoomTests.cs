aedd2303451c8cda9f22b1cc0263fef7d2a8c6cd
Aviv Gueta <guetaaviv@gmail.com>
2017-05-12 10:59:26 +0300

    add DoActionJoinTest2

--- TexasHoldemTests/Logic/Game/GameRoomTests.cs
+++ TexasHoldemTests/Logic/Game/GameRoomTests.cs
@@ -87,6 +87,17 @@ namespace TexasHoldem.Logic.Game.Tests
             Assert.IsTrue(gameRoom.DoAction(user2, ActionType.Join, 1000));
         }
 
+        [TestMethod()]
+        public void DoActionJoinTest2()
+        {
+            //already player user
+            Assert.IsFalse(gameRoom.DoAction(user1, ActionType.Join, 1000));
+            //new user to the game
+            Assert.IsTrue(gameRoom.DoAction(user2, ActionType.Join, 1000));
+        }
+
+                    SetDecoratoresLimitNoSpectatores();
+
 
         [TestMethod()]
         public void AddSpectetorToRoomTest()
