8428b20a45e458d0c53611597bc3c3fed2ffb253
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-13 10:05:34 +0300

    refactor League class - maybr not use any more

--- Logic/GameControl/League.cs
+++ Logic/GameControl/League.cs
@@ -3,50 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TexasHoldem.Logic.GameControl;
 
 namespace TexasHoldem.Logic.Game_Control
 {
     public class League
     {
-        private String name;
-        private int minRank;
-        private int maxRank;
-
-        public League(string name, int minRank, int maxRank)
+        private LeagueName name;
+        
+        public League(LeagueName _name)
         {
-            this.name = name;
-            this.minRank = minRank;
-            this.maxRank = maxRank;
+            this.name = _name;
+            
         }
 
 
         //getter setter name
-        public String getName()
+        public LeagueName getName()
         {
             return name;
         }
-        public void setName(String newName)
+        public void setName(LeagueName newName)
         {
             name = newName;
         }
-        //getter setter minRank
-        public int getMinRank()
-        {
-            return minRank;
-        }
-        public void setMinRank(int newMinRank)
-        {
-            minRank = newMinRank;
-        }
-        //getter setter maxRanl
-        public int getMaxRank()
-        {
-            return maxRank;
-        }
-        public void setMaxRank(int newMaxRank)
-        {
-            maxRank = newMaxRank;
-        }
+       
 
     }
 }
