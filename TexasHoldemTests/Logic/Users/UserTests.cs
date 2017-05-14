ffc24ef2e7ee25dcfd0ea1fea3b6193dcfde104a
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-14 23:49:03 +0300

    create new unit test for user

--- TexasHoldemTests/Logic/Users/UserTests.cs
+++ TexasHoldemTests/Logic/Users/UserTests.cs
@@ -13,14 +13,14 @@ namespace TexasHoldem.Logic.Users.Tests
     public class UserTests
     {
 
-        private User orelie = new User(305077901,"orelie","orelie26","123456",0,500,"orelie@post.bgu.ac.il");
-        private Notification toSend1 = new Notification(11,"joind");
+        private User orelie = new User(305077901, "orelie", "orelie26", "123456", 0, 500, "orelie@post.bgu.ac.il");
+        private Notification toSend1 = new Notification(11, "joind");
         private Notification toSend2 = new Notification(11, "Exit");
-       
+
         [TestMethod()]
         public void UserIdTest()
         {
-            Assert.AreEqual(orelie.Id(),305077901);
+            Assert.AreEqual(orelie.Id(), 305077901);
             Assert.AreNotEqual(orelie.Id(), 305077902);
         }
 
@@ -62,7 +62,7 @@ namespace TexasHoldem.Logic.Users.Tests
             Assert.AreNotEqual(orelie.Email(), "slafkjasp");
             Assert.AreNotEqual(orelie.Email(), "orelie@gmail.com");
         }
-       
+
 
         [TestMethod()]
         public void SendNotificationTest()
@@ -78,6 +78,257 @@ namespace TexasHoldem.Logic.Users.Tests
             Assert.IsTrue(orelie.AddNotificationToList(toSend1));
             Assert.IsTrue(orelie.AddNotificationToList(toSend2));
         }
-        
+
+        [TestMethod()]
+        public void UserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsUnKnowTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void IncGamesPlayTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendNotificationTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddNotificationToListTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MemberNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AvatarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PointsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoneyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WaitListNotificationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GamesAvailableToReplayTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ActiveGameListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SpectateGameListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WinNumTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IncWinNumTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LogoutTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditAvatarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserPointsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReduceMoneyIfPossibleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddMoneyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserMoneyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveRoomFromActiveGameListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveRoomFromSpectetorGameListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasThisActiveGameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasThisSpectetorGameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddRoomToActiveGameListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddRoomToSpectetorGameListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsLoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsValidPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetLeagueTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetLeagueTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HasEnoughMoneyTest()
+        {
+            Assert.Fail();
+        }
     }
 }
