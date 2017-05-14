64beaaa9eb33226c58cb49d939543fdcc08ec669
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-14 10:01:20 +0300

    chat message client to server

---
+++ TexasHoldemShared/CommMessages/ClientToServer/ChatCommMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldemShared.CommMessages.ClientToServer
+{
+    public class ChatCommMessage : CommunicationMessage
+    {
+        public override void Handle(IEventHandler handler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
