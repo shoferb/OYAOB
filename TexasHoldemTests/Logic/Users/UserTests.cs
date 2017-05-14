404fe6344f5fdb1b82d6867017a06039cc50b0c9
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-15 00:43:13 +0300

    EditAvatarTest_bad_empty1() - pass

--- TexasHoldemTests/Logic/Users/UserTests.cs
+++ TexasHoldemTests/Logic/Users/UserTests.cs
@@ -388,6 +388,13 @@ namespace TexasHoldem.Logic.Users.Tests
             Assert.IsTrue(user.EditAvatar("/GuiScreen/Photos/Avatar/Test.png")); ;
         }
 
+
+        [TestMethod()]
+        public void EditAvatarTest_bad_empty1()
+        {
+            IUser user = new User(305077901, "orelie", "orelie26", "123456789", 0, 500, "orelie@post.bgu.ac.il");
+            Assert.IsTrue(user.EditAvatar("")); ;
+        }
         [TestMethod()]
         public void EditUserPointsTest()
         {
