117033e90b541e1d61346629f84a894efad9f0b0
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-15 00:14:57 +0300

    EmailTest_Good() - pass

--- TexasHoldemTests/Logic/Users/UserTests.cs
+++ TexasHoldemTests/Logic/Users/UserTests.cs
@@ -238,9 +238,10 @@ namespace TexasHoldem.Logic.Users.Tests
         }
 
         [TestMethod()]
-        public void EmailTest()
+        public void EmailTest_Good()
         {
-            Assert.Fail();
+            IUser user = new User(305077901, "orelie", "orelie26", "123456789", 0, 500, "orelie@post.bgu.ac.il");
+            Assert.AreEqual(user.Email(), "orelie@post.bgu.ac.il");
         }
 
         [TestMethod()]
