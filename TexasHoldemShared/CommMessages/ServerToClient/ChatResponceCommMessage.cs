16a7cba54499ae2cbbac449c89d9eba336ea6d5a
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-14 09:51:40 +0300

    add chat responce server side

---
+++ TexasHoldemShared/CommMessages/ServerToClient/ChatResponceCommMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldemShared.CommMessages.ServerToClient
+{
+    public class ChatResponceCommMessage : ResponeCommMessage
+    {
+        int idReciver;
+        string senderngUsername;
+        ActionType chatType;
+        string msgToSend;
+
+
+    }
+}
