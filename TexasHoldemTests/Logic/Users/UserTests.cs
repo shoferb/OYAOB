985fd20841ae4963cc7c132c5c8ff3f44d30f26e
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-14 23:56:11 +0300

    IsUnKnowTestGood_on_at_10_Games() - pass

--- TexasHoldemTests/Logic/Users/UserTests.cs
+++ TexasHoldemTests/Logic/Users/UserTests.cs
@@ -93,6 +93,17 @@ namespace TexasHoldem.Logic.Users.Tests
         }
 
 
+        [TestMethod()]
+        public void IsUnKnowTestGood_on_at_10_Games()
+        {
+            IUser user = new User(305077901, "orelie", "orelie26", "123456", 0, 500, "orelie@post.bgu.ac.il");
+            for (int i = 0; i < 10; i++)
+            {
+                user.IncGamesPlay();
+            }
+            Assert.IsTrue(user.IsUnKnow());
+        }
+
 
         [TestMethod()]
         public void IncGamesPlayTest()
