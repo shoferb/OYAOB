0abc8a2ea3bc5032198a789915a766911e2b7fde
Aviv Gueta <guetaaviv@gmail.com>
2017-05-15 19:15:22 +0300

    SendMessageToClient new impl

--- Logic/GameControl/GameCenter.cs
+++ Logic/GameControl/GameCenter.cs
@@ -56,32 +56,8 @@ namespace TexasHoldem.Logic.Game_Control
             return gm.DoAction(user, action, amount);
         }
 
-        //TODO : add cuurPlayerTurn
         public void SendMessageToClient(Player player, GameDataCommMessage gmData, CommunicationMessage.ActionType action, bool isSucceed, List<int> idsToSend)
         {
-            //GameDataCommMessage gameDataMes = new GameDataCommMessage(player.user.Id(), roomId, player.getFirstCard(),
-            //    player.getSeconedCard(), gmData.getPublicCard(), gmData.getChips(),
-            //    gmData.getPotSize(), gmData.getPlayersNames(), gmData.getDealer(), gmData.GetBbPlayer(),
-            //    gmData.GetSbPlayer(), isSucceed); 
-            //switch (action)
-            //{
-            //    case CommunicationMessage.ActionType.HandCard:
-            //    case CommunicationMessage.ActionType.StartGame:
-            //        GameServiceHandler.SendMessageToClientGameData(gameDataMes);
-            //        break;
-
-            //    case CommunicationMessage.ActionType.Fold:
-            //    case CommunicationMessage.ActionType.Bet:
-            //    case CommunicationMessage.ActionType.Join:
-            //    case CommunicationMessage.ActionType.Leave:
-
-            //        // we need to send game message also
-            //        GameServiceHandler.SendMessageToClientGameData(gameDataMes);
-            //        ResponeCommMessage resp = new ResponeCommMessage(player.user.Id(), isSucceed, gameDataMes);
-            //        GameServiceHandler.SendMessageToClientResponse(resp);
-            //        break;
-            //}
-
 
         }
 
