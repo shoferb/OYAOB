6d85bbcba8c152ec1c382b4ebe71d8d642933b52
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-15 15:29:54 +0300

    HasThisActiveGameTest_good_dont_contain()

--- TexasHoldemTests/Logic/Users/UserTests.cs
+++ TexasHoldemTests/Logic/Users/UserTests.cs
@@ -429,6 +429,20 @@ namespace TexasHoldem.Logic.Users.Tests
         }
 
         [TestMethod()]
+        public void HasThisActiveGameTest_good_dont_contain()
+        {
+            IUser user = new User(305077901, "orelie", "orelie26", "123456789", 0, 1500, "orelie@post.bgu.ac.il");
+            IGame gameRoom;
+            int roomID = 9999;
+            List<Player> players = new List<Player>();
+            Player player1 = new Player(user, 1000, roomID);
+            players.Add(player1);
+            Decorator deco = SetDecoratoresNoLimitWithSpectatores();
+            gameRoom = new GameRoom(players, roomID, deco);
+            Assert.IsFalse(user.ActiveGameList().Contains(gameRoom));
+        }
+
+        [TestMethod()]
         public void HasThisSpectetorGameTest()
         {
             Assert.Fail();
