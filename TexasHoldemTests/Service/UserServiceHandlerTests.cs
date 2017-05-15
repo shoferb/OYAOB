fc9a7cc196b408386c2677234aff0957fa011345
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-15 18:39:23 +0300

    create UserServiceHandlerTests

---
+++ TexasHoldemTests/Service/UserServiceHandlerTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TexasHoldem.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.Service.Tests
+{
+    [TestClass()]
+    public class UserServiceHandlerTests
+    {
+        [TestMethod()]
+        public void LoginUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LogoutUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterToSystemTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUserByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserPointsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditMoneyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserNotificationsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserAvatarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetActiveGamesByUserNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSpectetorGamesByUserNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetIUserByUserNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserLeagueTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DevideLeagueTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
