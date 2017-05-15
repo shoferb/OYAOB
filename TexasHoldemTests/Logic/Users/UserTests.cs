a46c08c92e567db3f73049b56bfcdeba99281e94
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-05-15 15:39:45 +0300

    RemoveRoomFromActiveGameListTest_bad_dont_contain()

--- TexasHoldemTests/Logic/Users/UserTests.cs
+++ TexasHoldemTests/Logic/Users/UserTests.cs
@@ -417,6 +417,21 @@ namespace TexasHoldem.Logic.Users.Tests
             Assert.IsTrue(user.RemoveRoomFromActiveGameList(gameRoom));
         }
 
+
+        [TestMethod()]
+        public void RemoveRoomFromActiveGameListTest_bad_dont_contain()
+        {
+            IUser user = new User(305077901, "orelie", "orelie26", "123456789", 0, 1500, "orelie@post.bgu.ac.il");
+            IGame gameRoom;
+            int roomID = 9999;
+            List<Player> players = new List<Player>();
+            Player player1 = new Player(user, 1000, roomID);
+            players.Add(player1);
+            Decorator deco = SetDecoratoresNoLimitWithSpectatores();
+            gameRoom = new GameRoom(players, roomID, deco);
+            Assert.IsFalse(user.RemoveRoomFromActiveGameList(gameRoom));
+        }
+
         [TestMethod()]
         public void RemoveRoomFromSpectetorGameListTest()
         {
