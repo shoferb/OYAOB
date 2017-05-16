19ae1dfb74df9f6fcfbb074f9fd2707e6c9c84b4
shoferb <shoferb@post.bgu.ac.il>
2017-05-16 19:06:50 +0300

    fix replay MSG cTos

--- TexasHoldemShared/CommMessages/ClientToServer/ReplayCommMessage.cs
+++ TexasHoldemShared/CommMessages/ClientToServer/ReplayCommMessage.cs
@@ -8,16 +8,16 @@ namespace TexasHoldemShared.CommMessages.ClientToServer
 {
     public class ReplayCommMessage : CommunicationMessage
     {
-        public int gameId;
-        public bool isAllGames;
+        public int roomId;
+       
 
 
         public ReplayCommMessage() : base(-1) { } //for parsing
 
-        public ReplayCommMessage(int _userid, bool _isAll, int _gameId ) : base(_userid)
+        public ReplayCommMessage(int _userid, int _roomId ) : base(_userid)
         {
-            this.gameId = _gameId;
-            this.isAllGames = _isAll;
+            this.roomId = _roomId;
+           
         }
 
         public override bool Equals(CommunicationMessage other)
