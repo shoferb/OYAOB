651d6e07d2b615d7ac7ad43c0858fa5f29a75eb6
miamiHits <yardench@gmail.com>
2017-05-16 20:32:31 +0300

    unit tests client

---
+++ TexasHoldemTests/Logic/Client/ClientTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+
+namespace TexasHoldemTests.Logic.Client
+{
+    [TestClass()]
+    class ClientTests
+    {
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+           
+        }
+
+        [TestMethod()]
+
+        public void AddNewRoom()
+        {
+        }
+
+        [TestMethod()]
+
+        public void GameUpdateReceived()
+        {
+            
+        }
+
+        [TestMethod()]
+
+        public void SpectateRoom()
+        {
+           
+        }
+
+        [TestMethod()]
+
+        //needed to be call after create new ClientEventHandler and a new client logic
+        public void Init()
+        {
+           
+        }
+
+        [TestMethod()]
+
+        public void CloseSystem()
+        {
+           
+        }
+
+        [TestMethod()]
+
+        public void EditDetails()
+        {  
+        }
+
+        [TestMethod()]
+
+        public void JoinTheGame()
+        {
+           
+        }
+
+        [TestMethod()]
+
+        public void CreateNewRoom()
+        {//should ret int as the roomNumber
+          
+        }
+
+        [TestMethod()]
+
+        public void LeaveTheGame()
+        {
+
+        }
+
+        [TestMethod()]
+
+       public void SendChatMsg()
+        {
+            
+        }
+
+        [TestMethod()]
+
+        public void StartTheGame()
+        {
+        }
+
+        [TestMethod()]
+
+        public void Login()
+        {
+           
+        }
+
+        [TestMethod()]
+
+        public void AskForReplays()
+        {
+           
+        }
+
+        [TestMethod()]
+
+        public void Logout()
+        {
+            
+        }
+
+        [TestMethod()]
+
+        public void SearchGame()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void Register()
+        {
+       
+        }
+
+        [TestMethod()]
+
+        public void gotMsgTest()
+        {
+          
+        }
+
+        [TestMethod()]
+
+        public void NotifyChosenMoveTest()
+        {
+
+        }
+
+        [TestMethod()]
+
+        public void NotifyResponseReceived()
+        {
+           
+
+        }
+    }
+}
