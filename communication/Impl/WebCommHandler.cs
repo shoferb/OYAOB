f4a03b9d48bff316991f7d52fbd42dc8cc3c0c74
oded rojtblat <oded.rojtblat@gmail.com>
2017-05-22 00:12:21 +0300

    started web comm handler

---
+++ communication/Impl/WebCommHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using TexasHoldem.communication.Interfaces;
+using TexasHoldemShared.Parser;
+
+namespace TexasHoldem.communication.Impl
+{
+    class WebCommHandler : IWebCommHandler
+    {
+        private readonly HttpListener _listener;
+        private bool _shouldStop = false;
+        private static readonly string[] _prefixes = {"http://*:8080/"}; //TODO: maybe add more / change
+        private ICommMsgXmlParser _parser = new ParserImplementation();
+
+        public WebCommHandler()
+        {
+            _listener = new HttpListener();
+            foreach (var prefix in _prefixes)
+            {
+                _listener.Prefixes.Add(prefix);
+            }
+        }
+
+        private void Accept()
+        {
+            while (!_shouldStop)
+            {
+                try
+                {
+                    HttpListenerContext context = _listener.GetContext(); //blocks
+                    Task.Factory.StartNew(() => HandleContext(context));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("WebCommHandler::Accept _listener exception: " + e.Message);
+                }
+            }
+        }
+
+        private string HandleContext(HttpListenerContext context)
+        {
+            var request = context.Request;
+            var msgStr = new StreamReader(request.InputStream,
+                context.Request.ContentEncoding).ReadToEnd();
+            //TODO: handle msg with ServerEventHandler here
+            return "";
+        }
+
+        public void Start()
+        {
+            _listener.Start();
+            Accept();
+        }
+
+        public void Close()
+        {
+            _shouldStop = true;
+            _listener.Stop();
+        }
+    }
+}
