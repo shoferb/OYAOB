ea5bd8d5c70b8df42677333e2251ea2026c9df20
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-06-01 22:55:55 +0300

    user Db test class

---
+++ TexasHoldemTests/Database/UserDBTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TexasHoldem.Database;
+
+
+namespace TexasHoldemTests.Database
+{
+    [TestClass()]
+    public class UserDBTests
+    {
+
+
+        [TestMethod()]
+        public void GetAllUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserByUserNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddNewUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserHighestCashGainInGameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserIsActiveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserLeagueNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserMoneyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserNumOfGamesPlayedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserTotalProfitTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditUserWinNumTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
