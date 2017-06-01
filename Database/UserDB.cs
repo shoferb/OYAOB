22edb4b71e08e8dc7cfeca1d518d78fb0488a5f4
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-06-01 21:48:47 +0300

    user store procr=edure backup for git

--- Database/UserDB.cs
+++ Database/UserDB.cs
@@ -14,24 +14,52 @@ namespace TexasHoldem.Database
     public class UserDB
     {
         public List<userDatabaseOb> GetAllUser()
-
         {
-
-            using (IDbConnection db = new SqlConnection
-
+         using (IDbConnection db = new SqlConnection
             (ConfigurationManager.ConnectionStrings
-
                 ["DataBaseSadna"].ConnectionString))
-
             {
-
                 string readSp = "GetAllUser";
-
                 return db.Query<userDatabaseOb>(readSp,
-
                     commandType: CommandType.StoredProcedure).ToList();
-
             }
+        }
+
+        public userDatabaseOb GetUserById()
+        {
+            using (IDbConnection db = new SqlConnection
+            (ConfigurationManager.ConnectionStrings
+                ["DataBaseSadna"].ConnectionString))
+            {
+                string readSp = "GetUserByUserId";
+                return db.Query<userDatabaseOb>(readSp,
+                    commandType: CommandType.StoredProcedure).ToList().First();
+            }
+        }
+
+        public userDatabaseOb GetUserByUserName()
+        {
+            using (IDbConnection db = new SqlConnection
+            (ConfigurationManager.ConnectionStrings
+                ["DataBaseSadna"].ConnectionString))
+            {
+                string readSp = "GetUserByUserName";
+                return db.Query<userDatabaseOb>(readSp,
+                    commandType: CommandType.StoredProcedure).ToList().First();
+            }
+        }
+
+        public void AddNewUser(userDatabaseOb objUser)
+        {
+            //Passing stored procedure using Dapper.NET
+            using (IDbConnection db = new SqlConnection
+            (ConfigurationManager.ConnectionStrings
+                ["DataBaseSadna"].ConnectionString))
+            {
+                string readSp = "AddNewUser";
+                db.Execute(readSp, objUser, commandType: CommandType.StoredProcedure);
+            }
+
         }
     }
 }
