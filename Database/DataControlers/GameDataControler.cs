935a4dcefe5767458f86d39e09a6e14019fce5b1
shoferb <shoferb@post.bgu.ac.il>
2017-06-03 18:30:15 +0300

    using fixing

--- Database/DataControlers/GameDataControler.cs
+++ Database/DataControlers/GameDataControler.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TexasHoldem.Database.LinqToSql;
-using TexasHoldem.Database.DataControlers;
+
 
 namespace TexasHoldem.Database.DataControlers
 {
--- DatabaseProxy/GameDataProxy.cs
+++ DatabaseProxy/GameDataProxy.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TexasHoldem.Database.DataControlers;
+using TexasHoldem.Logic.Game;
 
 namespace TexasHoldem.DatabaseProxy
 {
