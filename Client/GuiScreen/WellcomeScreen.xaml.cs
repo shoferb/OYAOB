12534812f51cdec41dc9f73f4e380f87fbd564f6
orelie-PC\orelie <orelie@post.bgu.ac.il>
2017-06-07 20:56:01 +0300

    lod data control

--- Client/GuiScreen/WellcomeScreen.xaml.cs
+++ Client/GuiScreen/WellcomeScreen.xaml.cs
@@ -30,7 +30,7 @@ namespace TexasHoldem.GuiScreen
         {
             InitializeComponent();
             cl = new ClientLogic();
-            _commHandler = new ClientCommunicationHandler("192.168.43.53");
+            _commHandler = new ClientCommunicationHandler("172.20.10.12");
             _eventHandler = new ClientEventHandler(_commHandler);
             _eventHandler.Init(cl);
             cl.Init(_eventHandler, _commHandler);
--- Database/DataControlers/LogDataControler.cs
+++ Database/DataControlers/LogDataControler.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TexasHoldem.Database.LinqToSql;
 
 namespace TexasHoldem.Database.DataControlers
 {
-    class LogDataControler
+    public class LogDataControler
     {
+        public void AddErrorLog(ErrorLog error)
+        {
+            
+            try
+            {
+
+                using (connectionsLinqDataContext db = new connectionsLinqDataContext())
+                {
+                    //Log log = new Log();
+                    //log.LogId = error.logId;
+                    //log.PriorityLogEnum = error.Log.PriorityLogEnum;
+                    //db.Logs.InsertOnSubmit(log);
+                    db.Logs.InsertOnSubmit(error.Log);
+                    db.ErrorLogs.InsertOnSubmit(error);
+                    db.SubmitChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error in lod data control : error log insert fail");
+                return ;
+            }
+
+        }
+
+        public void AddSystemLog(SystemLog sysLog)
+        {
+
+            try
+            {
+                using (connectionsLinqDataContext db = new connectionsLinqDataContext())
+                {
+                    //Log log = new Log();
+                    //log.LogId = error.logId;
+                    //log.PriorityLogEnum = error.Log.PriorityLogEnum;
+                    //db.Logs.InsertOnSubmit(log);
+                    db.Logs.InsertOnSubmit(sysLog.Log);
+                    db.SystemLogs.InsertOnSubmit(sysLog);
+                    db.SubmitChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error in lod data control : error log insert fail");
+                return;
+            }
+
+        }
     }
 }
