285ff59163fc709482e1fd90f1868093fb5b90ca
oded rojtblat <oded.rojtblat@gmail.com>
2017-06-07 22:27:46 +0300

    commHandler switches socket if dossconnection

--- communication/Impl/ServerEventHandler.cs
+++ communication/Impl/ServerEventHandler.cs
@@ -45,25 +45,6 @@ namespace TexasHoldem.communication.Impl
             _sessionIdHandler = handler;
         }
 
-        private GameDataCommMessage GetGameDataForRoom(IGame room, ActionCommMessage msg, bool success)
-        {
-            Player player = room.GetPlayersInRoom().Find(p => p.user.Id() == msg.UserId);
-            Player currPlayer = room.GetCurrPlayer();
-            string currPlayerName = currPlayer != null ? currPlayer.user.MemberName() : "";
-            Player bb = room.GetBb();
-            string bbName = bb != null ? bb.user.MemberName() : "";
-            Player sb = room.GetSb();
-            string sbName = sb != null ? sb.user.MemberName() : "";
-            Player dealer = room.GetDealer();
-            string dealerName = dealer != null ? dealer.user.MemberName() : "";
-
-            return new GameDataCommMessage(msg.UserId, room.Id, msg.SessionId, player._firstCard, player._secondCard,
-                room.GetPublicCards(), player.TotalChip, room.GetPotSize(), 
-                room.GetPlayersInRoom().ConvertAll(p => p.user.MemberName()), 
-                dealerName, bbName, sbName, success, currPlayerName, 
-                player.user.MemberName(), msg.Amount, msg.MoveType);
-        }
-
         private ResponeCommMessage SendMessages(int userId, IEnumerator<ActionResultInfo> iterator, CommunicationMessage originalMsg)
         {
             ResponeCommMessage response = null;
@@ -104,8 +85,6 @@ namespace TexasHoldem.communication.Impl
             }
             return response;
         }
-
-
 
         public string HandleEvent(ActionCommMessage msg)
         {
