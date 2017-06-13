b7a06b31bea9733a38c89780ab0ec748721b8490
shoferb <shoferb@post.bgu.ac.il>
2017-06-13 15:09:37 +0300

    gamedata proxy done

--- DatabaseProxy/GameDataProxy.cs
+++ DatabaseProxy/GameDataProxy.cs
@@ -21,7 +21,7 @@ namespace TexasHoldem.DatabaseProxy
     {
 
         GameDataControler _controller;
-   
+
 
         public GameDataProxy()
         {
@@ -29,10 +29,10 @@ namespace TexasHoldem.DatabaseProxy
         }
 
         public bool AddNewGameToDB(Logic.Game.GameRoom gr)
-        { 
-           bool ans = false;
-           ans = InsertGameRoom(gr);
-           return ans;
+        {
+            bool ans = false;
+            ans = InsertGameRoom(gr);
+            return ans;
         }
 
         private bool InsertGameRoom(Logic.Game.GameRoom v)
@@ -43,7 +43,7 @@ namespace TexasHoldem.DatabaseProxy
             toIns.RoomId = v.Id;
             toIns.GameXML = GameRoomToXElement(v);
             toIns.Replay = v.GetGameReplay();
-           return  _controller.InsertGameRoom(toIns);
+            return _controller.InsertGameRoom(toIns);
         }
 
         private XElement GameRoomToXElement(object obj)
@@ -65,10 +65,10 @@ namespace TexasHoldem.DatabaseProxy
             return (Logic.Game.GameRoom)xmlSerializer.Deserialize(xElement.CreateReader());
         }
 
-       
+
         private bool InsertGamePref(Logic.Game.GameRoom v)
         {
-            Database.LinqToSql.GameRoomPreferance toAdd= new GameRoomPreferance();
+            Database.LinqToSql.GameRoomPreferance toAdd = new GameRoomPreferance();
             toAdd.GameId = v.GetGameNum();
             toAdd.BuyInPolicy = v.GetBuyInPolicy();
             toAdd.GameMode = _controller.GetGameModeValByName(v.GetGameMode().ToString());
@@ -90,8 +90,8 @@ namespace TexasHoldem.DatabaseProxy
         public List<IGame> GetAllGames()
         {
             List<IGame> toRet = new List<IGame>();
-            List<Database.LinqToSql.GameRoom> dbGames= _controller.getAllGames();
-            if(dbGames == null)
+            List<Database.LinqToSql.GameRoom> dbGames = _controller.getAllGames();
+            if (dbGames == null)
             {
                 return null;
             }
@@ -104,7 +104,7 @@ namespace TexasHoldem.DatabaseProxy
 
         private TexasHoldemShared.CommMessages.GameMode ConvertGameModeChosen(Database.LinqToSql.GameMode gameMode)
         {
-            if(gameMode.game_mode_name.Equals("Limit"))
+            if (gameMode.game_mode_name.Equals("Limit"))
             {
                 return TexasHoldemShared.CommMessages.GameMode.Limit;
             }
@@ -120,41 +120,9 @@ namespace TexasHoldem.DatabaseProxy
 
 
 
-        private Logic.Replay.GameReplay ConvertGameReplay(Database.LinqToSql.GameReplay repDB)
+        private Logic.GameControl.LeagueName ConvertLeague(Database.LinqToSql.LeagueName leagueDB)
         {
-            Logic.Replay.GameReplay toRet = new Logic.Replay.GameReplay();
-            toRet._gameRoomID = repDB.room_Id;
-            toRet._gameNumber = repDB.game_Id;
-            //TODO: string of actions???
-            return toRet;
-                
-        }
-
-        private Logic.Game.GameRoom.HandStep ConvertSpecsList(Database.LinqToSql.HandStep hsDB)
-        {
-            if (hsDB.hand_Step_name.Equals("Pre-Flop"))
-            {
-                return Logic.Game.GameRoom.HandStep.PreFlop;
-            }
-            else if (hsDB.hand_Step_name.Equals("River"))
-            {
-                return Logic.Game.GameRoom.HandStep.River;
-            }
-            else if (hsDB.hand_Step_name.Equals("Turn"))
-            {
-                return Logic.Game.GameRoom.HandStep.Turn;
-            }
-            else
-            {
-                return Logic.Game.GameRoom.HandStep.Flop;
-            }
-           
-        }
-
-
-        private Logic.GameControl.LeagueName ConvertSpecsList(Database.LinqToSql.LeagueName leagueDB)
-        {
-            if(leagueDB.League_Name.Equals("A"))
+            if (leagueDB.League_Name.Equals("A"))
             {
                 return Logic.GameControl.LeagueName.A;
             }
@@ -174,67 +142,11 @@ namespace TexasHoldem.DatabaseProxy
             {
                 return Logic.GameControl.LeagueName.E;
             }
-            else 
+            else
             {
                 return Logic.GameControl.LeagueName.Unknow;
             }
-
-
-
         }
 
-        private List<Logic.Users.Spectetor> ConvertSpecsList(List<Database.LinqToSql.SpectetorGamesOfUser> dbSpecs)
-        {
-            List<Logic.Users.Spectetor> toRet = new List<Logic.Users.Spectetor>();
-            foreach (Database.LinqToSql.SpectetorGamesOfUser s in dbSpecs)
-            {
-                //TODO
-                User user; //= UserDataProxy.GetUserById(dbPlayer.user_Id);
-                Logic.Users.Spectetor toAdd = new Logic.Users.Spectetor(/*user*/ null, s.roomId);
-            }
-            return toRet;
-        }
-
-        private List<Logic.Users.Player> ConvertPlayerList(List<Database.LinqToSql.Player> dbPlayers)
-        {
-            List<Logic.Users.Player> toRet = new List<Logic.Users.Player>();
-            foreach (Database.LinqToSql.Player dbPlayer in dbPlayers)
-            {
-                //TODO
-                User user; //= UserDataProxy.GetUserById(dbPlayer.user_Id);
-                Card fCard = getCardByVal(dbPlayer.first_card);
-                Card sCard = getCardByVal(dbPlayer.secund_card);
-                Logic.Users.Player toAdd = new Logic.Users.Player(/*IUser user*/ null, dbPlayer.Total_chip, dbPlayer.room_Id, dbPlayer.Round_chip_bet,
-                    dbPlayer.is_player_active, fCard, sCard, dbPlayer.Player_action_the_round);
-            }
-            return toRet;
-        }
-
-        private Card getCardByVal(int val)
-        {
-            Database.LinqToSql.Card dbCard = _controller.getDBCardByVal(val);
-            Suits s = new Suits();
-            if(dbCard.Card_Shpe.Equals("Clubs"))
-            {
-                s = Suits.Clubs;
-            }
-            else if (dbCard.Card_Shpe.Equals("Diamonds"))
-            {
-                s = Suits.Diamonds;
-            }
-            else if (dbCard.Card_Shpe.Equals("Hearts"))
-            {
-                s = Suits.Hearts;
-            }
-            else if (dbCard.Card_Shpe.Equals("Spades"))
-            {
-                s = Suits.Spades;
-            }
-            else if (dbCard.Card_Shpe.Equals("None"))
-            {
-                s = Suits.None;
-            }
-            return new Card(s, dbCard.Card_Real_Value);
-        }
-    }
+    }   
 }
