6b6bc61141f4ea6abd61697664abe6cfbd7f2825
oded rojtblat <oded.rojtblat@gmail.com>
2017-06-21 15:30:14 +0300

    both tests pass

--- TexasHoldemTests/communication/WebEventHandlerTest.cs
+++ TexasHoldemTests/communication/WebEventHandlerTest.cs
@@ -28,18 +28,6 @@ namespace TexasHoldemTests.communication
         private int _userId1 = -1;
         private int _userId2 = -1;
 
-        private void Init()
-        {
-            //LogControl lc = new LogControl();
-            //SystemControl sc = new SystemControl(lc);
-            //ReplayManager rm = new ReplayManager();
-            //GameCenter gc = new GameCenter(sc, lc, rm, _webEventHandler);
-            //_userService = new UserServiceHandler(gc, sc);
-            //_serverEventHandler = new ServerEventHandler(null, null, gc, sc, lc, rm, CommunicationHandler.GetInstance());
-            //_webEventHandler = new WebEventHandler(_serverEventHandler);
-            //_serverEventHandler.SetSessionIdHandler(_webEventHandler);
-        }
-
         [SetUp]
         public void Setup()
         {
@@ -49,64 +37,31 @@ namespace TexasHoldemTests.communication
         public void Teardown()
         {
             _serverEventHandler = null;
-            DeleteTheTwoUsers();
+            _webEventHandler = null;
         }
 
 
         //print jsons to show Yarden
-        [TestCase]
-        public void TestJson()
-        {
-            LeaderboardCommMessage lbcm = new LeaderboardCommMessage(1, -1, LeaderboardCommMessage.SortingOption.HighestCashGain);
-            var lineDatas = new List<LeaderboardLineData>
-            {
-                new LeaderboardLineData(1, "bla", 1000, 10, 20, 12),
-                new LeaderboardLineData(1, "blabla", 100, 120, 2, 132),
-                new LeaderboardLineData(1, "blablablabla", 10032, 12120, 23, 1432)
-            };
-
-            var res = new LeaderboardResponseCommMessage(lbcm.UserId, lbcm.SessionId, true, lbcm, lineDatas);
-            string xml = _parser.SerializeMsg(res, false);
-            string json = _parser.XmlToJson(xml);
-            Console.WriteLine(json);
-        }
-
-        //private void RegisterTwoUsers()
+        //[TestCase]
+        //public void TestJson()
         //{
-        //    Init();
-        //    Random rand = new Random();
-        //    _userId1 = rand.Next();
-        //    _userId2 = rand.Next();
-        //    _userService.RegisterToSystem(_userId1, "Oded", "Oded", "123456789", 1000, "bla@bla.com");
-        //    _userService.RegisterToSystem(_userId2, "Jordy", "Jordy", "123456789", 1000, "blabla@bla.com");
-        //    _userService.EditUserPoints(1, 100);
-        //    _userService.EditUserPoints(2, 1000);
-        //    _userService.GetUserById(_userId1).WinNum = 11;
-        //    _userService.GetUserById(_userId1).LoseNum = 1;
-        //    _userService.GetUserById(_userId1).TotalProfit = 1000;
-        //    _userService.GetUserById(_userId1).HighestCashGainInGame = 13;
-        //    _userService.GetUserById(_userId2).WinNum = 10;
-        //    _userService.GetUserById(_userId2).LoseNum = 1;
-        //    _userService.GetUserById(_userId2).TotalProfit = 10;
-        //    _userService.GetUserById(_userId2).HighestCashGainInGame = 130;
-        //}
+        //    LeaderboardCommMessage lbcm = new LeaderboardCommMessage(1, -1, LeaderboardCommMessage.SortingOption.HighestCashGain);
+        //    var lineDatas = new List<LeaderboardLineData>
+        //    {
+        //        new LeaderboardLineData(1, "bla", 1000, 10, 20, 12),
+        //        new LeaderboardLineData(1, "blabla", 100, 120, 2, 132),
+        //        new LeaderboardLineData(1, "blablablabla", 10032, 12120, 23, 1432)
+        //    };
 
-        private void DeleteTheTwoUsers()
-        {
-            if (_userId1 != -1)
-            {
-                _userService.DeleteUserById(_userId1);
-            }
-            if (_userId2 != -1)
-            {
-                _userService.DeleteUserById(_userId2);
-            }
-        }
+        //    var res = new LeaderboardResponseCommMessage(lbcm.UserId, lbcm.SessionId, true, lbcm, lineDatas);
+        //    string xml = _parser.SerializeMsg(res, false);
+        //    string json = _parser.XmlToJson(xml);
+        //    Console.WriteLine(json);
+        //}
 
         [TestCase]
         public void HandleRawMsgsLeaderBoardGood()
         {
-            //RegisterTwoUsers();
             LeaderboardCommMessage lbcm = new LeaderboardCommMessage(1, -1, LeaderboardCommMessage.SortingOption.HighestCashGain);
             var lineDatas = new List<LeaderboardLineData>
             {
@@ -120,37 +75,42 @@ namespace TexasHoldemTests.communication
 
             _webEventHandler = new WebEventHandler(_serverEventHandler.Object);
 
-            string expectedResponse = "r{ \"?xml\":{ \"@version\":\"1.0\"" +
-                                      ",\"@encoding\":\"utf-16\"},\"" +
-                                      "LeaderboardResponseCommMessage\":{ \"@xmlns:xsd\"" +
-                                      ":\"http://www.w3.org/2001/XMLSchema\",\"@xmlns:xsi\"" +
-                                      ":\"http://www.w3.org/2001/XMLSchema-instance\",\"UserId\"" +
-                                      ":\"1\",\"SessionId\":\"-1\",\"Success\":\"true\"," +
-                                      "\"OriginalMsg\":{ \"@xsi:type\":\"LeaderboardCommMessage\"" +
-                                      ",\"UserId\":\"1\",\"SessionId\":\"-1\",\"SortedBy\":" +
-                                      "\"HighestCashGain\"},\"Results\":{ \"LeaderboardLineData\"" +
-                                      ":[{\"Id\":\"1\",\"Name\":\"bla\",\"Points\":\"1000\",\"" +
-                                      "TotalGrossProfit\":\"10\",\"HighestCashGain\":\"20\"," +
-                                      "\"NumOfGamesPlayed\":\"12\"},{\"Id\":\"1\",\"Name\":" +
-                                      "\"blabla\",\"Points\":\"100\",\"TotalGrossProfit\":\"120\"" +
-                                      ",\"HighestCashGain\":\"2\",\"NumOfGamesPlayed\":\"132\"},{" +
-                                      "\"Id\":\"1\",\"Name\":\"blablablabla\",\"Points\":\"10032\"" +
-                                      ",\"TotalGrossProfit\":\"12120\",\"HighestCashGain\":\"23\"" +
-                                      ",\"NumOfGamesPlayed\":\"1432\"}]}}}";
-
+            string expectedResponse = "r{\"?xml\":{\"@version\":\"1.0\""+
+                                ",\"@encoding\":\"utf-16\"},\""+
+                                "LeaderboardResponseCommMessage\":{\"@xmlns:xsd\""+
+                                ":\"http://www.w3.org/2001/XMLSchema\",\"@xmlns:xsi\""+
+                                ":\"http://www.w3.org/2001/XMLSchema-instance\",\"UserId\""+
+                                ":\"1\",\"SessionId\":\"-1\",\"Success\":\"true\","+
+                                "\"OriginalMsg\":{\"@xsi:type\":\"LeaderboardCommMessage\""+
+                                ",\"UserId\":\"1\",\"SessionId\":\"-1\",\"SortedBy\":"+
+                                "\"HighestCashGain\"},\"Results\":{\"LeaderboardLineData\""+
+                                ":[{\"Id\":\"1\",\"Name\":\"bla\",\"Points\":\"1000\",\""+
+                                "TotalGrossProfit\":\"10\",\"HighestCashGain\":\"20\","+
+                                "\"NumOfGamesPlayed\":\"12\"},{\"Id\":\"1\",\"Name\":"+
+                                "\"blabla\",\"Points\":\"100\",\"TotalGrossProfit\":\"120\""+
+                                ",\"HighestCashGain\":\"2\",\"NumOfGamesPlayed\":\"132\"},{"+
+                                "\"Id\":\"1\",\"Name\":\"blablablabla\",\"Points\":\"10032\""+
+                                ",\"TotalGrossProfit\":\"12120\",\"HighestCashGain\":\"23\""+
+                                ",\"NumOfGamesPlayed\":\"1432\"}]}}}";
 
             var xml = _parser.SerializeMsg(lbcm, false);
             var json = _parser.XmlToJson(xml);
             var result = _webEventHandler.HandleRawMsg(json);
             Assert.NotNull(result);
             Assert.AreEqual(1, result.Count);
-            //Assert.True(expectedResponse.Equals(result[0]));
+            Assert.True(expectedResponse.Equals(result[0]));
         }
 
         [TestCase]
-        public void ParseStatisticsGood()
+        public void HandleRawMsgsStatisticsGood()
         {
-            //RegisterTwoUsers();
+            UserStatisticsCommMessage commMessage = new UserStatisticsCommMessage(1, -1);
+            _serverEventHandler = new Mock<IEventHandler>();
+            _serverEventHandler.Setup(e => e.HandleEvent(It.IsAny<UserStatisticsCommMessage>()))
+                .Returns(new UserStatisticsResponseCommMessage(commMessage.UserId, commMessage.SessionId,
+                true, commMessage, 83.333333333333329, 90.9090909090909));
+
+            _webEventHandler = new WebEventHandler(_serverEventHandler.Object);
 
             var expectedResponse =
                 "u{\"?xml\":{\"@version\":\"1.0\",\"@encoding\":\"utf-16\"}," +
@@ -159,7 +119,6 @@ namespace TexasHoldemTests.communication
                 "\"UserId\":\"1\",\"SessionId\":\"-1\",\"Success\":\"true\",\"OriginalMsg\":{\"@xsi:type\":\"UserStatisticsCommMessage\"," +
                 "\"UserId\":\"1\",\"SessionId\":\"-1\"},\"AvgCashGain\":\"83.333333333333329\",\"AvgGrossProfit\":\"90.9090909090909\"}}";
 
-            UserStatisticsCommMessage commMessage = new UserStatisticsCommMessage(1, -1);
             var xml = _parser.SerializeMsg(commMessage, false);
             var json = _parser.XmlToJson(xml);
             var result = _webEventHandler.HandleRawMsg(json);
