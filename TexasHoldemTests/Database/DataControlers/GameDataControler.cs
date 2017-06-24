edd27fdc78826f1fdd82204a7f3d754240bcb970
shoferb <shoferb@post.bgu.ac.il>
2017-06-24 21:52:08 +0300

    class for gamedata controller tests

---
+++ TexasHoldemTests/Database/DataControlers/GameDataControler.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TexasHoldem.Database.DataControlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.Database.DataControlers.Tests
+{
+    [TestClass()]
+    public class GameDataControler
+    {
+        [TestMethod()]
+        public void GameDataControlerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getAllGamesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertGameRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateGameRoomPotSizeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteGameRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteGameRoomPrefTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllActiveGameRoomsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllSpectatebleGameRoomsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomsByBuyInPolicyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomsByGameModeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomsByMinPlayersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomsByPotSizeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomsByStartingChipTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomsByMinBetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomsByMaxPlayersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomReplyByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameRoomPrefByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateGameRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertPrefTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetGameModeValByNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetLeagueValByNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllUserActiveGamesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserSpectetorsGameResultTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
